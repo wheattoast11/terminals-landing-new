104c3e9768e4da9256bb65b97ed40f9b0fd657d9
wheattoast11 <tdesai1994@gmail.com>
2025-02-16 13:44:39 -0600

    update deployment workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
-export default nextConfig;
+export default nextConfig
