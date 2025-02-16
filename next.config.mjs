36e5fe9489a368454c207af7e9139166c51e34ad
wheattoast11 <tdesai1994@gmail.com>
2025-02-16 13:47:13 -0600

    Fix Github Actions workflow location

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 export default nextConfig;
+// 
