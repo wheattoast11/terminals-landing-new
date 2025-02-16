b5bfdafb9ec942f1cb204d67e1943534f0269c91
wheattoast11 <tdesai1994@gmail.com>
2025-02-16 16:44:14 -0600

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
   },
